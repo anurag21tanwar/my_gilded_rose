e75097dbd634888396471eeaba1ddf7a4d9740b1
anurag21tanwar <anurag.tanwar@nuance.com>
2018-03-26 19:57:13 +0530

    - Adding refactored code for normal items. - Using quality.rb for processing quality and sell_in for a item. - Updating spec for normal item.

---
+++ lib/quality.rb
@@ -0,0 +1,29 @@
+module Quality
+
+  class Base
+    def update(item)
+      set_quality(item)
+      set_sell_in(item)
+    end
+
+    def set(item, value)
+      if item.quality >= 50
+        item.quality = 50
+      elsif item.quality <= 0
+        item.quality = 0
+      else
+        item.quality += value
+      end
+    end
+
+    def set_sell_in(item)
+      item.sell_in -= 1
+    end
+  end
+
+  class NormalItems < Quality::Base
+    def set_quality(item)
+      item.sell_in <= 0 ? set(item, -2) : set(item, -1)
+    end
+  end
+end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,30 +3,56 @@ require_relative '../lib/gilded_rose'
 describe "#update_quality" do
 
   context "with a single item" do
-    let(:initial_sell_in) { 5 }
-    let(:initial_quality) { 10 }
-    let(:name) { "item" }
     let(:item) { Item.new(name, initial_sell_in, initial_quality) }
 
     before { update_quality([item]) }
 
-    it "your specs here" do
-      pending
+    context "for normal item" do
+      let(:name) { "NORMAL ITEM" }
+      let(:initial_sell_in) { 5 }
+      let(:initial_quality) { 10 }
+
+      context "before sell date" do
+        it do
+          expect(item.quality).to eq(initial_quality - 1)
+        end
+      end
+
+      context "on zero sell date" do
+        let(:initial_sell_in) { 0 }
+        it do
+          expect(item.quality).to eq(initial_quality - 2)
+        end
+      end
+
+      context "after sell date" do
+        let(:initial_sell_in) { -1 }
+        it do
+          expect(item.quality).to eq(initial_quality - 2)
+        end
+      end
+
+      context "on zero quality" do
+        let(:initial_quality) { 0 }
+        it do
+          expect(item.quality).to eq(0)
+        end
+      end
     end
   end
 
   context "with multiple items" do
     let(:items) {
       [
-        Item.new("NORMAL ITEM", 5, 10),
-        Item.new("Aged Brie", 3, 10),
+          Item.new("NORMAL ITEM", 5, 10)
       ]
     }
 
     before { update_quality(items) }
 
-    it "your specs here" do
-      pending
+    it do
+      expect(items[0].quality).to eq(9)
+      expect(items[0].sell_in).to eq(4)
     end
   end
 end
